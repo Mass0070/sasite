0a83b75a319c8fb240acee8f1349e10b89221386
Mass0070 <52866257+Mass0070@users.noreply.github.com>
2020-08-22 00:34:40 +0200

    CSS-Update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,34 @@
 <template>
   <div>
-    <div class="stats">
-      <h1 class="Online">Online: <span>{{ players.online }}</span>/<span>{{ players.max }}</span></h1>
-      <p><span v-html="motd"></span></p>
-    </div>
+    <iframe align="right" src="https://canary.discordapp.com/widget?id=313097609948430336&theme=dark" width="18%" height="700" allowtransparency="true" frameborder="0"></iframe>
 
     <div class="Server-stats">
       <div class="Server-stats-module">
         <h1 class="Online">Online: <span>{{ players.online }}</span>/<span>{{ players.max }}</span></h1>
-        <p><span v-html="motd"></span></p>
+        <p class="Hjemmeside-Motd"><span v-html="motd"></span></p>
       </div>
     </div>
 
-    <img class="img-responsive">
-      <iframe align="right" src="https://canary.discordapp.com/widget?id=313097609948430336&theme=dark" width="18%" height="700" allowtransparency="true" frameborder="1"></iframe>
   </div>
   
 </template>
 
 <style>
+
+p.Hjemmeside-Motd {
+    margin-top: 2.5%;
+}
 .Server-stats {
-  max-width: 350px;
+  max-width: 20%;
   padding: 1rem;
   position: relative;
   background: linear-gradient(to right, blue, green);
-  padding: 4px;
+  padding: 0.3%;
   margin: auto;
-  margin-top: 20px;
+  margin-top: 2%;
   text-align: center; 
   box-sizing: content-box; 
 }
-
 .Server-stats-module {
   background: #222;
   color: white;
@@ -42,59 +40,18 @@
 .Online h1 {
   color: black; 
 }
-.stats {
-  background-color:#E1C699; 
-  margin: auto; 
-  margin-top: 10px;
-  text-align: center; 
-  box-sizing: content-box; 
-  width: 400px; 
-  height: auto; 
-  border-radius: 40px;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  padding-left: 5px;
-  padding-right: 5px;
-}
-.stats:hover {
-  background-color:#d8bb88; 
-  margin: auto; 
-  margin-top: 10px;
-  text-align: center; 
-  box-sizing: content-box; 
-  width: 400px; 
-  height: auto; 
-  border-radius: 40px;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  padding-left: 5px;
-  padding-right: 5px;
-}
 .Online {
   margin: auto;
   color: green;
 }
-#Discord{
-  color: white;
-  text-align: right;
-  background-color: #1f94e0;
-  font-size: 10px;
-  padding-right: 8em;
-  height: 5em;
-  line-height: 5em;
-  width: 18%;
-  margin-left: calc(100% - 18% - 10px);
-  border-radius: 7px;
-  }
 iframe{
-  margin-bottom: 30px;
-  margin-right: 10px;
-  -moz-border-radius: 12px;
-  -webkit-border-radius: 12px;
-  border-radius: 12px;
-  -moz-box-shadow: 4px 4px 14px #000;
-  -webkit-box-shadow: 4px 4px 14px #000;
-  box-shadow: 4px 4px 14px #000;
+  margin-right: 1.5%;
+  -moz-border-radius: 1.8%;
+  -webkit-border-radius: 1.8%;
+  border-radius: 1.8%;
+  -moz-box-shadow: 0.6% 0.6% 1.6% #000;
+  -webkit-box-shadow: 0.6% 0.6% 1.6% #000;
+  box-shadow: 0.6% 0.6% 1.6% #000;
   filter:progid:DXImageTransform.Microsoft.BasicImage(rotation=.2);
 } 
 </style>
