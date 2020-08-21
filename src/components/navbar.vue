72106b0f7c2c4ba36086a316b531ec0cf6fe14a3
Mass0070 <52866257+Mass0070@users.noreply.github.com>
2020-08-22 00:36:19 +0200

    Update navbar.vue

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -27,7 +27,7 @@ export default {
                     link: '/logout'
                 }
             ],
-            profile: true
+            profile: false
         }
     },
     async created() {
