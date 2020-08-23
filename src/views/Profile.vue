9c3019f24b5b1f74e21c6c3816c2527e93c0f482
Mass0070 <52866257+Mass0070@users.noreply.github.com>
2020-08-23 23:32:18 +0200

    CSS Updates
    Redesignet Profile.vue

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,24 +1,36 @@
 <template>
     <div class="Grid" v-if="info.length > 0">
         <div v-for="x in info" class="Unban" :key="x.id">
-            <p class="UnbanId"><a v-bind:href="'/ansøgning/'+ x.UnbanId">{{ x.UnbanId }}</a></p>
-            <p>Status: {{ x.status }}</p>
-            <p>Oprettet: {{ x.createdAt }}</p>
-            <p>Seneste opdateret: {{ x.updatedAt }}</p>
+            <p class="UnbanId"><a v-bind:href="'/ansøgning/'+ x.UnbanId"><span>ID:</span> {{ x.UnbanId }}</a></p>
+            <p class="P-Status">Status:<br><span>{{ x.status }}</span></p>
+            <p class="P-Oprettet">Oprettet:<br><span>{{ x.createdAt }}</span></p>
+            <p class="P-Opdateret">Seneste opdateret:<br><span>{{ x.updatedAt }}</span></p>
         </div>
     </div>
-    <div v-else-if="info.length = 0" id="zeroClass">
+    <div v-else-if="info.length <= 0" id="zeroClass">
       <div id="zeroClassdiv">
-        <h1>Du har ikke lavet nogen ansøgninger</h1>
+        <h1>Du har <span>ikke</span> lavet nogen ansøgninger</h1>
       </div>
     </div>
 </template>
 
 <style>
+p {
+  color: black;
+  font-size: 15px;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+  font-weight: bold;
+}
 #zeroClassdiv {
   margin: auto;
   text-align: center;
   color: #e6e6e6;
+}
+#zeroClassdiv span {
+  color: crimson;
 }
 #zeroClass {
   position: fixed;
@@ -32,30 +44,22 @@
   grid-template-columns: repeat(1, 1fr);
   grid-auto-rows: minmax(250px, auto);
 }
-a {
-  color: inherit;
-  text-decoration: none;
-  font-weight: bold;
-}
 .UnbanId {
-  font-size: 25px;
+  font-size: 20px;
   font-family: Arial, Helvetica, sans-serif;
   text-decoration: none;
+  margin-top: 3.5%;
 }
 .UnbanId:hover {
   opacity: 0.8
 }
 .Unban {
     margin: 0 auto;
-    background-color: #4CA3F4;
+    background-color: #3495ef;
     width: 225px;
     height: 225px;
     border-radius: 30px;
     text-align: center;
-}
-p {
-  color: black;
-  font-size: 15px;
 }
 .Grid {
     position: fixed;
@@ -68,6 +72,38 @@ p {
     grid-auto-rows: minmax(250px, auto);
     margin: 0 auto;
     text-align: left;
+}
+.Grid span {
+  color: #04ef04;
+  font-size: 110%;
+  font-family: "Times New Roman", Times, serif;
+}
+
+.P-Status {
+  font-size: 125%;
+  margin-top: 4%;
+}
+.P-Status span {
+  color: #04ef04;
+  font-family: "Times New Roman", Times, serif;
+}
+
+.P-Oprettet {
+  font-size: 95%;
+  margin-top: 4%;
+}
+.P-Oprettet span {
+  color: #04ef04;
+  font-family: "Times New Roman", Times, serif;
+}
+
+.P-Opdateret {
+  font-size: 95%;
+  margin-top: 4%;
+}
+.P-Opdateret span {
+  color: #04ef04;
+  font-family: "Times New Roman", Times, serif;
 }
 </style>
 
