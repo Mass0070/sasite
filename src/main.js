6409d4b0625e8b1c1171f13a599e8d0845bb6d80
itzoreomc <mhoff770@gmail.com>
2020-09-22 15:55:31 +0200

    Hmm :/

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VTooltip from 'v-tooltip'
+const vuetify = () => import(/* webpackChunkName: "group-Icons" */ `./plugins/vuetify`)
 
 Vue.config.productionTip = false
 Vue.use(VTooltip)
@@ -10,5 +11,6 @@ Vue.use(VTooltip)
 new Vue({
   router,
   store,
+  vuetify,
   render: function (h) { return h(App) }
 }).$mount('#app')
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+//import { VIcon } from 'vuetify/lib';
+
+//Vue.use(VIcon);
+
+export default new Vuetify({
+});
