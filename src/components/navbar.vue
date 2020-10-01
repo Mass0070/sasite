4b1bfe03c3fbca6268b8fb01957fd99fb14ae967
itzoreomc <mhoff770@gmail.com>
2020-10-01 19:53:28 +0200

    Fjern nogle mellemrum xD

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,17 +1,17 @@
 <template>
     <b-navbar toggleable="lg" type="dark" variant="info">
-            <b-navbar-brand href="/">SuperAwesome</b-navbar-brand>
-            <b-navbar-nav class="ml-auto">
-                <b-nav-item-dropdown v-if="profile === true" right>
-                    <template v-slot:button-content>
-                        <em>Profil</em>
-                    </template>
-                    <b-dropdown-item href="/profile">Profil</b-dropdown-item>
-                    <b-dropdown-item href="/logout">Log ud</b-dropdown-item>
-                </b-nav-item-dropdown>
-                <b-nav-item v-else href="https://discord.com/api/oauth2/authorize?client_id=694582426474774570&redirect_uri=https%3A%2F%2Fsuperawesome.ml%2Fauth%2F&response_type=token&scope=identify">Log ind</b-nav-item>
-            </b-navbar-nav>
-        </b-navbar>
+        <b-navbar-brand href="/">SuperAwesome</b-navbar-brand>
+        <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown v-if="profile === true" right>
+                <template v-slot:button-content>
+                    <em>Profil</em>
+                </template>
+                <b-dropdown-item href="/profile">Profil</b-dropdown-item>
+                <b-dropdown-item href="/logout">Log ud</b-dropdown-item>
+            </b-nav-item-dropdown>
+            <b-nav-item v-else href="https://discord.com/api/oauth2/authorize?client_id=694582426474774570&redirect_uri=https%3A%2F%2Fsuperawesome.ml%2Fauth%2F&response_type=token&scope=identify">Log ind</b-nav-item>
+        </b-navbar-nav>
+    </b-navbar>
 </template>
 
 <script defer>
