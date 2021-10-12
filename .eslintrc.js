39ef9e7a7cebf47951b9dd8cdce8c7886aef1045
Mig <mhoff770@gmail.com>
2021-10-12 13:13:49 +0200

    Maybe now! xd

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,35 @@
 module.exports = {
   root: true,
+
   env: {
     browser: true,
     node: true
   },
+
   parserOptions: {
     parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    requireConfigFile: false,
+    ecmaVersion: 2020
   },
+
   extends: [
     '@nuxtjs',
     'plugin:nuxt/recommended'
   ],
+
   plugins: [
   ],
+
   // add your custom rules here
-  rules: {}
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  },
+
+  'extends': [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'plugin:vue/essential',
+    '@vue/standard'
+  ]
 }
